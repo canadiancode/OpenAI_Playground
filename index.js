991c6b6ec3b58cefed88fbb3f78ecf468aea53f6
KevinHeidema <45132144+canadiancode@users.noreply.github.com>
2023-03-21 11:54:34 -0700

    Complete the request and return using OpenAI API

--- index.js
+++ index.js
@@ -1,50 +1,35 @@
-    // IMPORT DEPENDENCIES -- IMPORT DEPENDENCIES -- IMPORT DEPENDENCIES
-
-// import Express.js by requiring express
-const express = require('express');
-
-// require dotenv to import API key and run .config to load the API key
+// Require dotenv to import API key and run .config to load the API key
 require('dotenv').config();
+
+// Fetch the API key from the hidden folder
+const API_Key = process.env.OPEN_AI_KEY;
 
 // 2 imports from the OpenAI package
     // Configuration = object for defining things like API
     // OpenAIApi = provides an easy interface for interacting with the OpenAI API
 const { Configuration, OpenAIApi } = require('openai');
 
-
-    // SET UP A SERVER -- SET UP SERVER -- SET UP SERVER
-
-// create a server object called app and call the function using "()";
-const app = express();
-
-// adds middleware to run on every request to OpenAI which parses the body of the request into json format
-app.use(express.json());
-
-
-    // SET UP ENDPOINT -- SET UP ENDPOINT -- SET UP ENDPOINT
-
 // fetch the API key for the first argument in the post request 
 const configuration = new Configuration({
-    apiKey: process.env.OPEN_AI_KEY,
+    apiKey: API_Key,
 });
 
-// add the fethced API to the first argument of the post request
+// Add the fethced API to the first argument of the post request
 const openai = new OpenAIApi(configuration);
 
-// use app variable to listen for a post request
-app.post("/find-complexity", async (req, res) => {
-    try {
-        return res.status(200).json({
-            message: "Working",
-        });
-    } catch (error) { }
-});
+// Function to request OpenAI to run prompt
+const runPrompt = async () => {
 
+    const prompt = 'Tell me a joke about about a dog eating pasta';
 
-    // RUN SERVER -- RUN SERVER -- RUN SERVER
+    const response = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: prompt,
+        max_tokens: 2048,
+        temperature: 1
+    })
 
-// define a port for the server to run on
-const port = process.env.PORT || 3000;
+    console.log(response.data);
 
-// start the server
-app.listen(port, () => console.log(`Server listening on port ${port}`)); 
+};
+runPrompt();
