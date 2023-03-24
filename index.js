aab7e0a960c75888b75d05329727efb04be3ec2a
Kevin Heidema <heidemakevin@gmail.com>
2023-03-23 17:22:31 -0700

    start of the web scraper

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // packages installed:
-    // dotenv:  for keeping the API keys hidden
-    // openai:  post prompt to OpenAI to summerize articles 
-    // axios:   promise-based HTTPS requests using node.js 
+    // dotenv:    for keeping the API keys hidden
+    // axios:     promise-based HTTPS requests using node.js
+    // puppeteer: headless browser for extracting data from news
+    // openai:    post prompt to OpenAI to summerize articles 
 
 // Require dotenv to import API keys and run .config to load the API keys into the index.js file 
 require('dotenv').config();
@@ -26,17 +27,24 @@ const options = {
 };
 
 // retrieve the URL's for the top 20 trending news stories from coindesk
-
+let article_Title_Array = [];
 let article_URL_Array = [];
+
+// retrieve the URL's from Rapid API
 axios.request(options).then(function (response) {
 
+    // remove old Titles and URL's from the previous day
+    article_Title_Array = [];
     article_URL_Array = [];
 
+    // loop through fetch data and push the URL's into an array
     response.data.forEach(data => {
+        console.log(data);
         let url = data.url;
         article_URL_Array.push(url)
-        console.log(article_URL_Array);
     });
+
+    // console.log(article_URL_Array);
 
 }).catch(function (error) {
 	console.error(error);
@@ -45,7 +53,16 @@ axios.request(options).then(function (response) {
 
 // SCRAPE ARTICLE CONTENT --  SCRAPE ARTICLE CONTENT -- SCRAPE ARTICLE CONTENT
 
+// const puppeteer = require('puppeteer');
 
+// async function scrapeArticle(url) {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto(url);
+
+// };
+
+// scrapeArticle('https://www.coindesk.com/business/2023/03/23/crypto-exchange-coinbase-shares-tumble-16-after-sec-enforcement-notice/?utm_medium=referral&utm_source=rss&utm_campaign=headlines');
 
 
 // SUMMERIZE WITH OPENAI -- SUMMERIZE WITH OPENAI -- SUMMERIZE WITH OPENAI
